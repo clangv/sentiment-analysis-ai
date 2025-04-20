377d4be9a0f439532f4c8c93384fd5863b04c737
clangv <issayvega@gmail.com>
2025-04-20 22:20:00 +0800

    Overall Sentiment in gold

--- app.py
+++ app.py
@@ -3,6 +3,38 @@ import feedparser
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 
+ticker = 'GC=F'
+keyword = 'gold'
+
 pipe = pipeline("text-classification", model="ProsusAI/finbert")
 
-print(pipe('Stocks rallied and the British pound gained.'))
+rss_url = f'https://finance.yahoo.com//rss/headline?s={ticker}'
+
+feed = feedparser.parse(rss_url)
+
+total_score = 0
+num_article = 0
+
+for i, entry in enumerate (feed.entries):
+    if keyword.lower() not in entry.summary.lower():
+        continue
+
+    print(f'Title: {entry.title}')
+    print(f'Link: {entry.link}')
+    print(f'Published: {entry.published}')
+    print(f'Summary: {entry.summary}')
+
+    sentiment = pipe(entry.summary)[0]
+
+    print(f'Sentiment {sentiment ["label"]}, Score')
+    print('-' * 40)
+
+    if sentiment ['label'] == 'positive':
+        total_score += sentiment['score']
+        num_article += 1
+    elif sentiment ['label'] == 'negative':
+        total_score -= sentiment['score']
+        num_article += 1
+
+final_score = total_score / num_article
+print(f'Overall Sentiment: {"Positive" if final_score >= 0.15 else "Negative" if final_score <= -0.15 else "Neutral"} {final_score}')
