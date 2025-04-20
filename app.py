1dca5f9a718a5af9903f35e1ceef1a2bc01ddfd3
clangv <issayvega@gmail.com>
2025-04-20 16:07:28 +0800

    update

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
- 
+import feedparser
+
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+
+pipe = pipeline("text-classification", model="ProsusAI/finbert")
+
+print(pipe('Stocks rallied and the British pound gained.'))
